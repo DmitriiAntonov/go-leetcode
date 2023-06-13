3ec99e1bfa60c7c47728172f896aabbe9eb29fcf
dmitriiantonov <dmitriiantonov90@icloud.com>
2023-06-13 15:52:32 +0200

    Fixed golint

--- problem71/simplify_path.go
+++ problem71/simplify_path.go
@@ -1,4 +1,4 @@
-package simplify_path
+package problem71
 
 import "strings"
 
@@ -7,15 +7,14 @@ func simplifyPath(path string) string {
 
 	for _, part := range parts {
 		switch part {
-		case ".", "":
-		// Do nothing
 		case "..":
 			if stack.Len() != 0 {
 				stack.Pop()
 			}
 		default:
-			stack.Push(part)
-
+			if part != "." && part != "" {
+				stack.Push(part)
+			}
 		}
 	}
 
@@ -25,7 +24,7 @@ func simplifyPath(path string) string {
 
 	sb := strings.Builder{}
 
-	for _, component := range stack.data {
+	for _, component := range stack.Slice() {
 		sb.WriteString("/")
 		sb.WriteString(component)
 	}
@@ -33,14 +32,17 @@ func simplifyPath(path string) string {
 	return sb.String()
 }
 
+// Stack is implemented stack data structure
 type Stack struct {
 	data []string
 }
 
+// Push method add value to the stack
 func (s *Stack) Push(value string) {
 	s.data = append(s.data, value)
 }
 
+// Pop method returns a last added value and removes it from the stack
 func (s *Stack) Pop() string {
 	if s.Len() == 0 {
 		panic("The stack is empty")
@@ -51,10 +53,12 @@ func (s *Stack) Pop() string {
 	return value
 }
 
+// Len method returns a length of the internal slice
 func (s *Stack) Len() int {
 	return len(s.data)
 }
 
+// Slice method returns the internal slice
 func (s *Stack) Slice() []string {
 	return s.data
 }
